419ef503ba6ee8adeb07582d76ff8ed42b38bb4a
Trey Hayden <33755714+vbhayden@users.noreply.github.com>
2021-12-29 15:52:31 -0500

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -3,14 +3,15 @@
 echo "" >> ~/renew.log
 echo "Checking $1 for SSL renewals ..." >> ~/renew.log
 
-docker-compose -f $1/docker-compose.yml run certbot \
+cd $1
+docker-compose run certbot \
 	renew --webroot \
 	--register-unsafely-without-email \
 	--agree-tos \
 	--no-random-sleep-on-renew \
 	--webroot-path=/data/letsencrypt >> ~/renew.log
 
-docker-compose -f $1/docker-compose.yml restart nginx
+docker-compose restart nginx
 
 echo "----------------------------------" >> ~/renew.log
 echo "last run:" >> ~/renew.log
